7c46eb9c0dafd26bef1196f9cb7731f20adbd3da
Nickolas Sharp <nickolas.sharp@welocalize.com>
2017-03-20 12:40:28 -0700

    cleanup to dot to add get and set for light JSON path

--- handlers/DynamoTtlEnforcer/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-// a gentleman to run on a timer in CloudWatch Events
-// who will force the actual delete of expired TTL items
-// in case we see that Dynamo's best effort isn't quite good enough
-// this assumes that our Enforcer would be granted
-// higher priority than the dynamo background TTL guy
---
+++ handlers/Scheduler/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+/*
+  - Runs on a schedule like ActivityPoller
+  - works its way through a list of things to trigger
+  - same contstraints as ActivityPoller as far as working its way through
+*/
